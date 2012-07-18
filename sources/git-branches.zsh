d905c61903c0a6dedc7010109423a37f10d7ad00
Toshiki Teramura <toshiki.teramura@gmail.com>
2012-07-18 23:39:26 +0900

    add merge action

--- sources/git-branches.zsh
+++ sources/git-branches.zsh
@@ -5,8 +5,8 @@ function zaw-src-git-branches() {
     if [[ $? == 0 ]]; then
         : ${(A)candidates::=${${(f)"$(git show-ref | awk '{ print $2}' |sed '/^refs\/stash$/d')"}#refs/}}
     fi
-    actions=(zaw-src-git-branches-checkout zaw-src-git-branches-create zaw-src-git-branches-delete)
-    act_descriptions=("check out" "create new branch from..." "delete")
+    actions=(zaw-src-git-branches-checkout zaw-src-git-branches-merge zaw-src-git-branches-merge-to zaw-src-git-branches-create zaw-src-git-branches-delete)
+    act_descriptions=("check out" "merge" "merge to" "create new branch from..." "delete")
     options=()
 }
 
@@ -26,6 +26,23 @@ function zaw-src-git-branches-create () {
     local b_name=${1#(heads|remotes|tags)/}
     LBUFFER="git checkout -b "
     RBUFFER=" $b_name"
+}
+
+function zaw-src-git-branches-merge () {
+    local b_type=${1%%/*}
+    local b_name=${1#(heads|remotes|tags)/}
+    BUFFER="git merge --no-ff $b_name"
+    zle accept-line
+}
+
+function zaw-src-git-branches-merge-to () {
+    local b_type=${1%%/*}
+    local b_name=${1#(heads|remotes|tags)/}
+    local b_now=${$(git symbolic-ref HEAD)#refs/heads/}
+    if [[ "$b_type" == "heads" ]]; then
+        BUFFER="git checkout $b_name && git merge --no-ff $b_now"
+        zle accept-line
+    fi
 }
 
 function zaw-src-git-branches-delete () {
