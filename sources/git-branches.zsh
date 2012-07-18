3d660ca79595b8b9a01d918b393aa903c5389055
Toshiki Teramura <toshiki.teramura@gmail.com>
2012-07-18 21:36:55 +0900

    add "create new branch" action

--- sources/git-branches.zsh
+++ sources/git-branches.zsh
@@ -1,9 +1,12 @@
 # zaw source for git branch
 
 function zaw-src-git-branches() {
-    : ${(A)candidates::=${${(f)"$(git show-ref | awk '{print $2}')"}#refs/(heads|remotes)/}}
-    actions=(zaw-src-git-branches-checkout zaw-src-git-branches-delete)
-    act_descriptions=("check out" "delete")
+    git rev-parse --git-dir >/dev/null 2>&1
+    if [[ $? == 0 ]]; then
+        : ${(A)candidates::=${${(f)"$(git show-ref | awk '{print $2}')"}#refs/(heads|remotes)/}}
+    fi
+    actions=(zaw-src-git-branches-checkout zaw-src-git-branches-create zaw-src-git-branches-delete)
+    act_descriptions=("check out" "create new branch from..." "delete")
     options=(-n)
 }
 
@@ -16,6 +19,11 @@ function zaw-src-git-branches-checkout () {
         BUFFER="git checkout -t $1"
         zle accept-line
     fi
+}
+
+function zaw-src-git-branches-create () {
+    LBUFFER="git checkout -b "
+    RBUFFER=" $1"
 }
 
 function zaw-src-git-branches-delete () {
